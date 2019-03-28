abc3641cfe51289e5d185443f7ee833cba685b83
Feriixu <feriixu@users.noreply.github.com>
2019-03-28 12:49:43 +0100

    Initial commit

---
+++ Captcha Solver/Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace Captcha_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(703, 126);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.LargeChange = 1;
+            this.trackBar1.Location = new System.Drawing.Point(12, 199);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(703, 45);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(12, 250);
+            this.trackBar2.Maximum = 100;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(703, 45);
+            this.trackBar2.TabIndex = 2;
+            this.trackBar2.Scroll += new System.EventHandler(this.TrackBar2_Scroll);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(640, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 437);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Captcha Solver/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Captcha_Solver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap bmpRaw = new Bitmap(@"captcha.png");
+            pictureBox1.BackgroundImage = bmpRaw;
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.BackgroundImage = CleanBitmap(bmpRaw, 1, 0.5f, 4);
+        }
+
+        private static Bitmap CleanBitmap(Bitmap raw, int thickness, float noiseThresh, int neighThresh)
+        {
+            Bitmap dirty = new Bitmap(raw);
+            for (int x = 0; x < dirty.Width; x++)
+            {
+                for (int y = 0; y < dirty.Height; y++)
+                {
+                    Color pixCol = dirty.GetPixel(x, y);
+                    if (pixCol.GetBrightness() > noiseThresh)
+                    {
+                        dirty.SetPixel(x, y, Color.White);
+                    }
+                    else
+                    {
+                        for (int i = -thickness; i < 1; i++)
+                        {
+                            for (int j = -thickness; j < 1; j++)
+                            {
+                                if ((x + i > dirty.Width) || (x + i < 0) || (y + j > dirty.Height) || (y + j < 0))
+                                {
+                                    continue;
+                                }
+                                else
+                                {
+                                    dirty.SetPixel(x + i, y + j, Color.Black);
+                                }
+                            }
+                        }
+                    }
+
+                    if (x == 0 || x == dirty.Width - 1 || y == 0 || y == dirty.Height - 1)
+                    {
+                        dirty.SetPixel(x, y, Color.Black);
+                    }
+                }
+            }
+
+            for (int x = 1; x < dirty.Width - 1; x++)
+            {
+                for (int y = 1; y < dirty.Height - 1; y++)
+                {
+                    int neigh = 0;
+                    for (int i = -1; i <= 1; i++)
+                    {
+                        for (int j = -1; j <= 1; j++)
+                        {
+                            if (dirty.GetPixel(x + i, y + j).GetBrightness() < noiseThresh)
+                            {
+                                neigh++;
+                            }
+                        }
+                    }
+
+                    if (neigh > neighThresh)
+                    {
+                        dirty.SetPixel(x, y, Color.Black);
+                    }
+                }
+            }
+
+            return new Bitmap(dirty);
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = CleanBitmap(new Bitmap(@"captcha.png"), trackBar1.Value, trackBar2.Value / 100.0f, 4);
+        }
+
+        private void TrackBar2_Scroll(object sender, EventArgs e)
+        {
+            pictureBox1.BackgroundImage = CleanBitmap(new Bitmap(@"captcha.png"), trackBar1.Value, trackBar2.Value / 100.0f, 4);
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Bitmap cleaned = new Bitmap(pictureBox1.BackgroundImage);
+            cleaned.Save(@"captcha_cleaned.png");
+        }
+    }
+}
